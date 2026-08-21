c12de8eb406f60c8a5537ae21be90171c497dc22
Konner McNeillie <konner.mcneillie@icloud.com>
2026-08-21 08:00:35 +0100

    fix: rm code

--- src/package/__init__.py
+++
@@ -1,6 +0,0 @@
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("research-package")  # Must match the exact name in pyproject.toml
-except PackageNotFoundError:
-    __version__ = "unknown"
